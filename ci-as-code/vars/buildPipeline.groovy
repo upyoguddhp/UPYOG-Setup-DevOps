38d3a4ae6b81b3b8914a4fda9c2290a5995303dc
upyoguddhp <noreply.upyoguddhp@gmail.com>
2026-02-20 15:21:48 +0530

    Switch deployment container from kubectl to kaniko

--- ci-as-code/vars/buildPipeline.groovy
+++ ci-as-code/vars/buildPipeline.groovy
@@ -192,17 +192,20 @@ spec:
                                 """
                                 echo "${image} pushed successfully!"
                                 }
-                                container('kubectl') {
-                                  sh """
-                                      echo "Deploying ${buildConfig.getImageName()}"
+                                container('kaniko') {
+                                    sh """
+                                        apk add --no-cache curl
+                                        curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+                                        chmod +x kubectl
+                                        mv kubectl /usr/local/bin/
 
-                                      kubectl set image deployment/${buildConfig.getImageName()} \
-                                      ${buildConfig.getImageName()}=${image} \
-                                      -n egov
+                                        kubectl set image deployment/${buildConfig.getImageName()} \
+                                        ${buildConfig.getImageName()}=${image} \
+                                        -n egov
 
-                                      kubectl rollout status deployment/${buildConfig.getImageName()} -n egov
-                                  """
-                              }                                
+                                        kubectl rollout status deployment/${buildConfig.getImageName()} -n egov
+                                    """
+                                }                               
                             }
                         }
                     }
